0896d0b0ea35097537915e913c32af0e35bd89c8
Temitayo <linuxmathew1245@gmail.com>
2025-04-02 09:15:15 +0100

    add: id to project array to  uniquely identify each

--- src/app/home/work/workCard.jsx
+++ src/app/home/work/workCard.jsx
@@ -1,44 +1,57 @@
 import Image from "next/image";
+import { FaArrowRight } from "react-icons/fa";
+// import "./workCard.css";
 
-const WorkCard = ({ image, title, link, desc }) => {
+const WorkCard = ({ image, title, link, desc, addLink }) => {
+  console.log("WorkCard props:", { image, title, link, desc });
   return (
-    <div
-      className="relative group"
-      style={{
-        padding: "30px",
-        backgroundColor: "#13132F",
-        borderRadius: "24px",
-        overflow: "hidden", // Ensure content stays within rounded borders
-      }}
-    >
-      {/* Image with fade-out on hover */}
-      <Image
-        src={image}
-        alt={title}
-        width={330}
-        height={250}
-        layout="responsive"
-        className="object-cover w-full h-60 transition-opacity duration-300 group-hover:opacity-20"
-      />
-
-      {/* Overlay background that appears on hover */}
+    <>
       <div
-        className="absolute inset-0 bg-[#13132F] opacity-0 group-hover:opacity-90 transition-opacity duration-300"
-        style={{ zIndex: 10 }} // Overlay on top of image
-      />
-
-      {/* Description that appears on hover */}
-      <div
-        className="absolute inset-0 flex items-center justify-center p-4 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300"
-        style={{ zIndex: 20 }} // Description on top of overlay
+        className="relative group"
+        style={{
+          padding: "30px",
+          backgroundColor: "#13132F",
+          borderRadius: "24px",
+          overflow: "hidden", // Ensure content stays within rounded borders
+          marginBottom: "24px",
+        }}
       >
-        <p className="text-sm text-center text-white font-medium ">
-          {desc || "No description available"}
-        </p>
-      </div>
+        {/* Image with fade-out on hover */}
+        <Image
+          src={image}
+          alt={title}
+          width={330}
+          height={250}
+          layout="responsive"
+          className="object-cover w-full h-60 transition-opacity duration-300 group-hover:opacity-20"
+        />
 
-      {/* Optional: Uncomment to show title and link below image at all times */}
-      {/* <div className="p-4 bg-white">
+        {/* Overlay background that appears on hover */}
+        <div
+          className="absolute inset-0 bg-[#13132F] opacity-0 group-hover:opacity-90 transition-opacity duration-300"
+          style={{ zIndex: 10 }}
+        />
+        <div
+          className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+          style={{
+            zIndex: 20,
+            backdropFilter: "blur(5px)", // Apply blur to the background
+            backgroundColor: "rgba(0, 0, 0, 0.5)", // Optional dark overlay
+          }}
+        >
+          <p className="text-sm text-center text-white font-medium">{desc}</p>
+        </div>
+        {/* <div
+          className="absolute inset-0 flex items-center justify-center p-4 opacity-0 transition-opacity duration-300 description-overlay mt-0"
+          style={{ zIndex: 20 }}
+        >
+          <p className="text-sm text-center text-white font-medium bg-red-500">
+            {desc}
+          </p>
+        </div> */}
+
+        {/* Optional: Uncomment to show title and link below image at all times */}
+        {/* <div className="p-4 bg-white">
         <h3 className="text-lg font-bold">{title}</h3>
         {link && (
           <a
@@ -51,7 +64,26 @@ const WorkCard = ({ image, title, link, desc }) => {
           </a>
         )}
       </div> */}
-    </div>
+      </div>
+      {addLink && (
+        <p className="d-flex" style={{ cursor: "pointer" }}>
+          {" "}
+          <span
+            style={{
+              color: "var(--icon-color)",
+              marginRight: "20px",
+              width: "32px",
+              height: "32px",
+            }}
+          >
+            <FaArrowRight />{" "}
+          </span>{" "}
+          <span style={{ color: "var(--text-color)", fontWeight: "32px" }}>
+            {title}
+          </span>
+        </p>
+      )}
+    </>
   );
 };
 
--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,28 +1,33 @@
+import SectionDivider from "../components/SectionDivider";
 import SlashHead from "../components/slashHead/slashHead";
 import WorkCard from "../home/work/workCard";
 
 export default function Work() {
   const projects = [
     {
+      id: 1,
       image: "/images/project1.png", // Ensure the public folder in Next.js
-      title: "Research Gains",
+      title: "Klakpad Sella",
       link: "https://example.com/research-gains",
       desc: "This is a brief description of the project consectetur. Varius blandit facilisis egestas nulla tristique.  sit amet consectetur. blandit facilisis egestas sit amet consectetur facilisis egestas nulla consectetur.  blandit facilisis egestas nulla tristique. ",
     },
 
     {
+      id: 2,
       image: "/images/project3.png", // Ensure the public folder in Next.js
-      title: "Research Gains",
+      title: "Klakpad website",
       link: "https://example.com/research-gains",
       desc: "This is a brief description of the project consectetur. Varius blandit facilisis egestas nulla tristique.  sit amet consectetur. blandit facilisis egestas sit amet consectetur facilisis egestas nulla consectetur.  blandit facilisis egestas nulla tristique.  ",
     },
     {
+      id: 3,
       image: "/images/project4.png",
-      title: "Fyuur",
+      title: "ResearchGains",
       link: "https://example.com/fyuur",
       desc: "This is a brief description of the project consectetur. Varius blandit facilisis egestas nulla tristique.  sit amet consectetur. blandit facilisis egestas sit amet consectetur facilisis egestas nulla consectetur.  blandit facilisis egestas nulla tristique.  ",
     },
     {
+      id: 4,
       image: "/images/project2.gif",
       title: "Fyuur",
       link: "https://example.com/fyuur",
@@ -31,26 +36,29 @@ export default function Work() {
     // Add more projects here
   ];
   return (
-    <div className="container-md" style={{ marginTop: "8rem" }}>
-      <SlashHead
-        headName="work"
-        desc="From product based teams and startups I’ve worked with"
-      />
+    <>
+      <div className="container-md" style={{ marginTop: "8rem" }}>
+        <SlashHead
+          headName="work"
+          desc="From product based teams and startups I’ve worked with"
+        />
 
-      <div className="row gy-3">
-        {projects.map((item, index) => (
-          <div className="col col-12 col-md-6">
-            <WorkCard
-              image={item.image}
-              title={item.title}
-              link={item.link}
-              desc={item.desc}
-              key={index}
-            />
-          </div>
-        ))}
+        <div className="row gy-5">
+          {projects.map((item, index) => (
+            <div className="col col-12 col-md-6" key={item.id}>
+              <WorkCard
+                image={item.image}
+                title={item.title}
+                link={item.link}
+                desc={item.desc}
+                addLink={true}
+              />
+            </div>
+          ))}
+        </div>
+        {/* <p>This page showcases my projects and accomplishments.</p> */}
       </div>
-      {/* <p>This page showcases my projects and accomplishments.</p> */}
-    </div>
+      <SectionDivider isDefault />
+    </>
   );
 }
