12fb7893691c8f3e2b192fa15829138df97ef78c
Temitayo <linuxmathew1245@gmail.com>
2025-04-16 09:42:42 +0100

    add: work page header showing timeline, role and tools

--- src/app/work/[uuid]/page.jsx
+++ src/app/work/[uuid]/page.jsx
@@ -15,12 +15,78 @@ const WorkPage = () => {
         </p>
       </div>
       <div className="row">
-        <div className="col">
-          Led the design efforts for the mobile app & website, working closely
-          with a with a fellow designer, the founder, and the engineering team.
+        <div className="col col-12 col-md-5">
+          <p
+            className="text-muted"
+            style={{
+              fontSize: "12px",
+              fontWeight: "var(--font-weight-medium)",
+            }}
+          >
+            ROLE/TEAM
+          </p>
+          <p
+            className="role-title-desc"
+            style={{
+              fontWeight: "var(--font-weight-regular)",
+              fontSize: "16px",
+              color: "var(--resume-headline)",
+            }}
+          >
+            Led the design efforts for the mobile app & website, working closely
+            with a with a fellow designer, the founder, and the engineering
+            team.
+          </p>
         </div>
-        <div className="col">weeks</div>
-        <div className="col">Restful API, Node Js, React Js,</div>
+        <div className="project-timeline col col-12 col-md-3">
+          <p
+            className="text-muted"
+            style={{
+              fontSize: "12px",
+              fontWeight: "var(--font-weight-medium)",
+            }}
+          >
+            TIMELINE
+          </p>
+          <p
+            style={{
+              fontWeight: "var(--font-weight-regular)",
+              fontSize: "16px",
+              color: "var(--resume-headline)",
+            }}
+          >
+            weeks
+          </p>
+          <p
+            style={{
+              fontWeight: "var(--font-weight-regular)",
+              fontSize: "16px",
+              color: "var(--resume-headline)",
+            }}
+          >
+            Year: June 2022
+          </p>
+        </div>
+        <div className="col col-12 col-md-4 ">
+          <p
+            className="text-muted"
+            style={{
+              fontSize: "12px",
+              fontWeight: "var(--font-weight-medium)",
+            }}
+          >
+            TOOLS USED
+          </p>
+          <p
+            style={{
+              fontWeight: "var(--font-weight-regular)",
+              fontSize: "16px",
+              color: "var(--resume-headline)",
+            }}
+          >
+            Restful API, Node Js, React Js,
+          </p>
+        </div>
       </div>
     </div>
   );
