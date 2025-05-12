d8265b9cfc555b7e973797886e186504c15b0918
Temitayo <linuxmathew1245@gmail.com>
2025-05-12 21:49:18 +0100

    add: fetchapi to article page

---
+++ src/app/shelf/services/fetchAllArticles.js
@@ -0,0 +1,21 @@
+import { sanityClient } from "../../../lib/sanity";
+
+export async function getAllArticles() {
+  try {
+    const articles = await sanityClient.fetch(
+      `*[_type == "article" && defined(slug.current)] | order(publishedAt desc){
+            title,
+            "slug": slug.current,
+            "image": image.asset->url,
+            publishedAt,
+            readTime,
+            excerpt, 
+            tip
+            }`
+    );
+    return articles;
+  } catch (error) {
+    console.error("Error fetching all articles:", error);
+    return [];
+  }
+}
